a69c34547229297c29d31007b29dea7cb423bcaa
rick <philipesferreiraa@gmail.com>
2023-04-03 23:12:31 -0300

    lendo os arquivos xlms e acessando o banco de dados

---
+++ script.js
@@ -0,0 +1,104 @@
+
+const xlsx = require("xlsx");
+const mysql = require('mysql');
+
+const wb = xlsx.readFile("tabelabehappy.xlsx");
+
+const ws = wb.Sheets["BE HAPPY"]
+
+console.log(ws)
+
+
+const connection = mysql.createConnection({
+    host: '67.23.238.59',
+    user: 'behappytabacaria_root',
+    password: 'n1WM9^I4A{Y{',
+    database: 'behappytabacaria_hlg'
+});
+
+async function runTransaction() {
+    try {
+        // Start a transaction
+        await beginTransaction(connection);
+
+        // Insert a new record into the "users" table
+         const userId = await insertUser(connection, 'behappytabacaria_hlg', 'behappytabacaria_root');
+         console.log('Inserted record with ID ' + userId);
+
+        // Commit the transaction
+        await commitTransaction(connection);
+
+        console.log('Transaction committed successfully.');
+    } catch (error) {
+        console.error('Error performing transaction: ' + error.stack);
+
+        // Rollback the transaction
+        await rollbackTransaction(connection);
+
+        console.error('Transaction rolled back.');
+    } finally {
+        connection.end();
+    }
+}
+
+async function beginTransaction(connection) {
+    return new Promise((resolve, reject) => {
+        connection.beginTransaction((error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function commitTransaction(connection) {
+    return new Promise((resolve, reject) => {
+        connection.commit((error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function rollbackTransaction(connection) {
+    return new Promise((resolve, reject) => {
+        connection.rollback((error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function insertUser(connection, name, email) {
+    const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    const values = [name, email];
+
+    return new Promise((resolve, reject) => {
+        connection.query(sql, values, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.insertId);
+            }
+        });
+    });
+}
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected to MySQL database as ID ' + connection.threadId);
+
+    runTransaction();
+});
